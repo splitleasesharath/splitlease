5b454b7a111c5d940a57a1ed6a25fc929b915f35
splitleasesharath <splitleasesharath@gmail.com>
2026-01-16 07:54:55 -0500

    Codebase Cleanup

--- supabase-migrations/backups/20260110/temp_import_commands.sql
+++
@@ -1,3 +0,0 @@
-﻿SET session_replication_role = replica;
-\i 'C:/Users/Split Lease/Documents/Split Lease/supabase-migrations/backups/20260110/production_data.sql'
-SET session_replication_role = DEFAULT;
